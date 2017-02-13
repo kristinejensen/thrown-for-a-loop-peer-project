b3b4452e4b7bc2f3ecd4d74d981df326c2ead2ea
Kristine Jensen <kristinejensen@Kristines-MacBook-Pro.local>
2017-02-13 16:46:07 -0600

    got pro mode to work

--- client.js
+++ client.js
@@ -1,23 +1,48 @@
 $(document).ready(function() {
 
-var employeeArray = [{name: "Tony", yearsOfExperience: 10}, {name: "Carla", yearsOfExperience: 4}, {name: "Kris", yearsOfExperience: 10}];
+  var employeeArray = [{name: "Tony", yearsOfExperience: 10}, {name: "Carla", yearsOfExperience: 4}, {name: "Kris", yearsOfExperience: 10}];
 
-console.log(experienceTotaler(employeeArray));
+  $('#submitButton').on('click', function(){
+    var employeeName = $('#employeeName').val();
+    var yearsOfExperience = parseFloat($('#yearsOfExperience').val());
+    var newEmployeeObject = new Employee(employeeName, yearsOfExperience);
+    employeeArray.push(newEmployeeObject);
 
-function experienceTotaler(array){
-  var totalExperience = 0;
-  array.forEach(function(employee){
-    totalExperience += employee.yearsOfExperience;
+    $('#employeeInformation').append(
+      '<tr>' +
+      '<td>' + employeeName + '</td>' +
+      '<td>' + yearsOfExperience + '</td>' +
+      '</tr>'
+    );
+
+    $('#totalYearsExperience').text(experienceTotaler(employeeArray));
   });
-  return totalExperience;
-}
 
-// $('#employeeInformation').append('<td>'employee name</td>
-//   <td>years of experience</td>
+  employeePrintOut(employeeArray);
+  $('#totalYearsExperience').text(experienceTotaler(employeeArray));
 
-);
+  function Employee(name, yearsOfExperience) {
+    this.name = name;
+    this.yearsOfExperience = yearsOfExperience;
+  };
 
+  function experienceTotaler(array){
+    var totalExperience = 0;
+    array.forEach(function(employee){
+      totalExperience += employee.yearsOfExperience;
+    });
+    return totalExperience;
+  }
 
-
+  function employeePrintOut(array){
+    array.forEach(function(employee){
+      $('#employeeInformation').append(
+        '<tr>' +
+        '<td>' + employee.name + '</td>' +
+        '<td>' + employee.yearsOfExperience + '</td>' +
+        '</tr>'
+      );
+    });
+  }
 
 });
